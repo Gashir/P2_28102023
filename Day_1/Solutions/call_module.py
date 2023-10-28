17cf67d95ff245ad472e7357325ea0e96edd374d
Vadim <vadimx30@gmail.com>
2023-10-28 15:28:09 +0300

    Added 01_distance solution

---
+++ Day_1/Solutions/call_module.py
@@ -0,0 +1,7 @@
+import sol_01_distance as sol
+
+
+tochka_one = sol.Point(1, 1)
+tochka_two = sol.Point(4, 5)
+result = sol.distance(tochka_one, tochka_two)
+print('Расстояние:', result)
--- Day_1/Solutions/sol_01_distance.py
+++ Day_1/Solutions/sol_01_distance.py
@@ -4,15 +4,23 @@ class Point:
         self.y = y
 
 
-def distance(p1, p2):
+# Используем подсказки типов(hinting) в сигнатуре функции
+def distance(p1: Point, p2: Point) -> float:
     """ Расстояние между двумя точками """
+    result = ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+    return result
 
 
+def bad_distance(p1, p2):
+    """ Расстояние между двумя точками """
+    return ((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2) ** 0.5
 
-# Дано две точки на координатной плоскости
-point1 = Point(2, 4)
-point2 = Point(5, -2)
 
-# Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
-# TODO: your code here...
-print("Расстояние между точками = ", ...)
+if __name__ == '__main__':
+    # Дано две точки на координатной плоскости
+    point1 = Point(2, 4)
+    point2 = Point(5, -2)
+
+    # Задание: Найдите расстояние между этими точками. Реализовав и используя функцию distance()
+    print("Расстояние между точками = ", distance(point1, point2))
+    print("Расстояние между точками = ", bad_distance(point1, point2))
