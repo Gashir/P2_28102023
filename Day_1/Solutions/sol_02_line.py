4c4231daf3788883c490237c621ddfae612d75ec
Vadim <vadimx30@gmail.com>
2023-10-28 16:44:51 +0300

    Added 02_line solution

--- Day_1/Solutions/sol_02_line.py
+++ Day_1/Solutions/sol_02_line.py
@@ -13,6 +13,29 @@ def distance(p1: Point, p2: Point) -> float:
 points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
 
 # Задание: Найдите длину ломаной линии
-# TODO: your code here...
+# First step(algorithm, private case)
+AB = distance(points[0], points[1])
+BC = distance(points[1], points[2])
+CD = distance(points[2], points[3])
+DE = distance(points[3], points[4])
+length_line = AB + BC + CD + DE
+print("Длина ломаной линии = ", length_line)
 
-print("Длина ломаной линии = ", ...)
+# Second step(universal algorithm)
+# Variant 1
+length = 0
+for index in range(len(points) - 1):
+    length += distance(points[index], points[index + 1])
+
+print("Длина ломаной линии = ", length)
+
+# Variant 2
+lengths = []
+for index in range(1, len(points)):
+    lengths.append(distance(points[index - 1], points[index]))
+
+print("Длина ломаной линии = ", sum(lengths))
+
+# Variant 3
+pts = points
+print("Длина ломаной линии = ", sum([distance(pts[i], pts[i+1]) for i in range(len(pts)-1)]))
