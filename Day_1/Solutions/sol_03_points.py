a1e2bb3bf51c11e20b358fc57adb8e5bc16a3416
Vadim <vadimx30@gmail.com>
2023-10-28 17:17:28 +0300

    Finish Day_1

--- Day_1/Solutions/sol_03_points.py
+++ Day_1/Solutions/sol_03_points.py
@@ -4,9 +4,9 @@ class Point:
         self.y = y
 
 
-def distance(p1, p2):
+def distance(p1: Point, p2: Point) -> float:
     """ Расстояние между двумя точками """
-    pass
+    return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
     
     
 # Дан список точек
